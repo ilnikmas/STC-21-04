124dd9db2098c2e17f74ca198130aa6803396c72
Ilya Maslenchenko <ilnikmas@tut.by>
2021-03-03 19:46:52 +0300

    новая ветка

--- src/main/java/ru/innopolis/ilnikmas/stc/Main.java
+++ src/main/java/ru/innopolis/ilnikmas/stc/Main.java
@@ -3,5 +3,6 @@ package ru.innopolis.ilnikmas.stc;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println("This is my feature");
     }
 }
