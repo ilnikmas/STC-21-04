17d3146cb06843bbcd2622159e016eb7488d20b1
Ilya Maslenchenko <ilnikmas@tut.by>
2021-05-03 17:53:03 +0300

    Выполнено задание урока 22

--- DynamicWorker.java
+++
@@ -1,7 +0,0 @@
-package ru.innopolis.ilnikmas.stc.lesson10;
-
-public class DynamicWorker implements Worker {
-
-    @Override
-    public void doWork() { System.out.println("Hello!");}
-}
---
+++ src/main/java/ru/innopolis/ilnikmas/stc/lesson22/connection/ConnectionManagerJDBCImpl.java
@@ -0,0 +1,37 @@
+package ru.innopolis.ilnikmas.stc.lesson22.connection;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * Класс, реализующий подключение к БД с заданными параметрами
+ */
+
+public class ConnectionManagerJDBCImpl implements ConnectionManager {
+    private static final Logger logger = LogManager.getLogger(ConnectionManagerJDBCImpl.class);
+    public static final ConnectionManager INSTANCE = new ConnectionManagerJDBCImpl();
+    private static final String DB_URL = "jdbc:postgresql://localhost:5432/postgres";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "postgres";
+
+    private ConnectionManagerJDBCImpl(){
+    }
+
+    public static ConnectionManager getInstance(){return INSTANCE;}
+
+    @Override
+    public Connection getConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(DB_URL, USER, PASSWORD);
+            logger.info("Connection to database established");
+        } catch (SQLException e) {
+            logger.error("Connection error: {}", e.getMessage());
+        }
+        return connection;
+    }
+}
